a453a2bb6c816f5fc39b415466a41fdcbb73a237
Julia <julia.hebauf@gmx.de>
2024-12-18 12:45:06 +0100

    Fix response of getLocation (the wrapper object now returns "location" instead of "locations")

--- backend/src/lambda/http/location/getLocation.ts
+++ backend/src/lambda/http/location/getLocation.ts
@@ -37,7 +37,7 @@ const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Pro
   return {
     statusCode: 200,
     body: JSON.stringify({
-      locations: result
+      location: result
     })
   }
 };
